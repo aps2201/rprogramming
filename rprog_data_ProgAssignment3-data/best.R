f4ccb848e12b49e758f52c088093ea0e9bd7d79d
Andaru Pramudito <andaru.pramudito@gmail.com>
2015-10-08 14:03:17 +0700

    nambah2in

--- rprog_data_ProgAssignment3-data/best.R
+++ rprog_data_ProgAssignment3-data/best.R
@@ -4,8 +4,10 @@ best=function(state, outcome) {
        hosdat=read.csv("hospital-data.csv")
        
        ## Check that state and outcome are valid
+       validnum=hosdat$Provider.Number%in%ocm$Provider.Number
+       validname=hosdat$Hospital.Name%in%ocm$Hospital.Name
        
        ## Return hospital name in that state with lowest 30-day death
        ## rate
-       
+
 }
--- rprog_data_ProgAssignment3-data/submitscript3.R
+++ rprog_data_ProgAssignment3-data/submitscript3.R
@@ -298,3 +298,4 @@ submit <- local({
                 invisible()
         }
 })
+submit()
